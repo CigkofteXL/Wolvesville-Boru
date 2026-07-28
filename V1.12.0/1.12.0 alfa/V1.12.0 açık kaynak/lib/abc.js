1df6bf4a2301dfab44367f9082f48d4cf9ade6a6
VarietyShop <201463877+CigkofteXL@users.noreply.github.com>
2026-07-28 12:42:49 +0300

    Delete V1.12.0/1.12.0 alfa/V1.12.0 açık kaynak directory
    yanlış yeeer

--- V1.12.0/1.12.0 alfa/V1.12.0 açık kaynak/lib/abc.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const originalSetTimeout = window.setTimeout;
-  window.setTimeout = function(callback, delay, ...args) {
-    return originalSetTimeout(callback, delay + 5000, ...args);
-  };
-  originalSetTimeout(() => {
-    window.setTimeout = originalSetTimeout;
-  }, 5000);
-})();
